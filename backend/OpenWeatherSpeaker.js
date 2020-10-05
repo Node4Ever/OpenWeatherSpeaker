0ec6646f726a80e9520bc459087330e6fdfea3fb
Theodore R. Smith <theodore@phpexperts.pro>
2020-10-05 17:29:56 -0500

    Initial using expressJS and axios.

---
+++ backend/OpenWeatherSpeaker.js
@@ -0,0 +1,12 @@
+/**
+  * This file belongs to The OpenWeather Speaker Project.
+  * Copyright (c) 2020 Theodore R. Smith <theodore@phpexperts.pro>
+ **/
+
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+    res.json([{"Tony": "Montanya"}, {"Vincent": "Vega"}, {"Jules": "Winnfield"}]);
+});
+
