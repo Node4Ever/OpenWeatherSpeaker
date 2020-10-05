a3c4a9bb1c655b7351706391b648db46c5f60a75
Theodore R. Smith <theodore@phpexperts.pro>
2020-10-05 17:32:02 -0500

    Securely store the OpenWeather API key using dotenv.

--- backend/OpenWeatherSpeaker.js
+++ backend/OpenWeatherSpeaker.js
@@ -3,8 +3,11 @@
   * Copyright (c) 2020 Theodore R. Smith <theodore@phpexperts.pro>
  **/
 
+require('dotenv').config();
 const express = require("express");
 const app = express();
+
+const OPENWEATHER_APIKEY = process.env.OPENWEATHER_APIKEY;
 
 app.listen(3000, () => {
     res.json([{"Tony": "Montanya"}, {"Vincent": "Vega"}, {"Jules": "Winnfield"}]);
