44515237bd370db3f68e26a7ef60d32325c55446
Theodore R. Smith <theodore@phpexperts.pro>
2020-10-05 19:23:20 -0500

    Initial OpenWeather client: Fetch 5-day forecast.

--- backend/OpenWeatherSpeaker.js
+++ backend/OpenWeatherSpeaker.js
@@ -4,12 +4,47 @@
  **/
 
 require('dotenv').config();
+const axios = require('axios');
 const express = require("express");
 const app = express();
 
 const OPENWEATHER_APIKEY = process.env.OPENWEATHER_APIKEY;
+const OPENWEATHER_HOST = "https://api.openweathermap.org";
 
-app.listen(3000, () => {
-    res.json([{"Tony": "Montanya"}, {"Vincent": "Vega"}, {"Jules": "Winnfield"}]);
+app.get('/', function (req, res) {
+    res.send('Hello, World!');
 });
 
+function grabDailyForecast(dailyData)
+{
+    if (!dailyData || !dailyData.list || !dailyData.list[0].dt_txt) {
+        throw "Couldn't parse the OpenWeather API data. Did the API change?";
+    }
+
+    dailyData.list.forEach(threeHourSummary => {
+        const date = new Date(threeHourSummary.dt);
+        console.log(threeHourSummary.dt + ' => ' + threeHourSummary.dt_txt + ' vs ' + date.getDate())
+        // date.getUTCHours()
+        // date.getDate();
+    });
+}
+
+
+app.get("/weather/:city/5-day", function (req, res) {
+    const city = req.params.city;
+    const apiURL = `${OPENWEATHER_HOST}/data/2.5/forecast?q=${city}&appid=${OPENWEATHER_APIKEY}`;
+
+    axios.get(apiURL).then(response => {
+        // res.send(response.data);
+        grabDailyForecast(response.data);
+        res.send(JSON.stringify(response.data, null, 4));
+    })
+    .catch(error => {
+        res.send({
+            "error": "Something went wrong",
+            "details": error
+        });
+    });
+});
+
+app.listen(3000);
